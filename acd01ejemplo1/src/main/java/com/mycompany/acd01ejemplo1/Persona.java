b74e48fb91c24f857763e6d3adde5ee5bcb50b3d
Daniel Ojeda <dmojeda@utpl.edu.ec>
2023-04-20 10:55:08 -0500

    clases

---
+++ acd01ejemplo1/src/main/java/com/mycompany/acd01ejemplo1/Persona.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.acd01ejemplo1;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Persona {
+    
+    //1. Declarar mis atributos 
+    
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String identificacion;
+    
+    //2. Métodos 
+    /*1 click izq
+     *2 insert code
+     *3 get and setter
+     */
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+    
+    
+}
